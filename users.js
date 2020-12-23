67cb828835a7f1c2117b9612fde5ab70f5f5cf65
HasnainAsif <63241977+HasnainAsif@users.noreply.github.com>
2020-12-23 10:29:49 +0500

    Update users.js

--- users.js
+++ users.js
@@ -1,49 +1,58 @@
 const allUsers = [
-    {
-        id: 100,
-        name: "Qasim Salam"
-    },
-        {
-        id: 101,
-        name: "Neelam Anwar"
-    },
-        {
-        id: 102,
-        name: "Saad Javaid"
-    },
-        {
-        id: 103,
-        name: "Talha Masood"
-    },
-        {
-        id: 104,
-        name: "Ali Bilal"
-    },
-        {
-        id: 105,
-        name: "Zain"
-    },
-    {
-        id: 106,
-        name: "Talal Ali"
-    },
-        {
-        id: 107,
-        name: "Bilal Zain"
-    },
-        {
-        id: 108,
-        name: "Mueez Ali"
-    },
+  {
+    id: 100,
+    name: "Qasim Salam",
+  },
+  {
+    id: 101,
+    name: "Neelam Anwar",
+  },
+  {
+    id: 102,
+    name: "Saad Javaid",
+  },
+  {
+    id: 103,
+    name: "Talha Masood",
+  },
+  {
+    id: 104,
+    name: "Ali Bilal",
+  },
+  {
+    id: 105,
+    name: "Zain",
+  },
+  {
+    id: 106,
+    name: "Talal Ali",
+  },
+  {
+    id: 107,
+    name: "Bilal Zain",
+  },
+  {
+    id: 108,
+    name: "Mueez Ali",
+  },
 ];
 
 const linked = [
-    {
-        id: 106,
-        name: "Talal Ali"
-    },
-    {
-        id: 108,
-        name: "Mueez Ali"
-    },
-]
+  {
+    id: 106,
+    name: "Talal Ali",
+  },
+  {
+    id: 108,
+    name: "Mueez Ali",
+  },
+];
+
+const linkedData = (allUsers, linked) => {
+  let unLinked = allUsers;
+  linked.forEach((item) => {
+    unLinked = unLinked.filter((user) => user.id !== item.id);
+  });
+  console.log(unLinked);
+};
+linkedData(allUsers, linked);
